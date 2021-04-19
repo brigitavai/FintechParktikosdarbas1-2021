40736954df2878d3d72598175b7caa97c1c54ddc
Brigita <vaiciulyte.brigita@gmail.com>
2021-04-19 22:21:18 +0300

    Added second button

--- app/src/main/java/com/example/fintech1parktikosdarbas2021/MainActivity.java
+++ app/src/main/java/com/example/fintech1parktikosdarbas2021/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.fintech1parktikosdarbas2021;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ButtonBarLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tvMain;
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +20,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.tvMain = findViewById(R.id.tvMain);
+        this.b1 = (Button) findViewById(R.id.btnMain2);
     }
 
     public void OnBtnclick(View view) {
         this.tvMain.setText(R.string.changed_label_caption);
+    }
+    public void OnBtnclick2(View view) {
+        this.tvMain.setTextColor(Color.GREEN);
     }
 }
